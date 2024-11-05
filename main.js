5712cc77bd94c91e6f2e8ca164cce6cd02c21746
Yura <ud@emy>
2024-11-05 12:27:10 +0200

    first

---
+++ main.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const japaneseRestaurant = {
+    name: 'Banzai',
+    location: '108 Markham Woods Rd, Longwood, USA',
+    categories: ['Japanese', 'Sushi', 'Vegetarian', 'Organic'],
+    appetizers: ['Seaweed salad', 'Tempura shrimp', 'Edamame', 'Sushi rice'],
+    mainMenu: ['Sushi', 'Ramen', 'Tempura'],
+
+    orderFood(appetizersIndex, mainMenuIndex) {
+        return [
+            this.appetizers[appetizersIndex],
+            this.mainMenu[mainMenuIndex]
+        ]
+    }
+};
+
+const [appetizer, main] = japaneseRestaurant.orderFood(3, 1)
+
+const myArr = [5, 1, [6, 4]]
+const [,,[a,b]] = myArr
+console.log(a,b);
