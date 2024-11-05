a3f1f7af1f74a5fc9c0b53b9d74d78f3c2406b09
Yura <ud@emy>
2024-11-05 12:40:28 +0200

    fjs

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
 'use strict';
 
 const japaneseRestaurant = {
-    name: 'Banzai',
+    restName: 'Banzai',
     location: '108 Markham Woods Rd, Longwood, USA',
     categories: ['Japanese', 'Sushi', 'Vegetarian', 'Organic'],
     appetizers: ['Seaweed salad', 'Tempura shrimp', 'Edamame', 'Sushi rice'],
     mainMenu: ['Sushi', 'Ramen', 'Tempura'],
-
+    workingHours: {
+        wed: {
+            open: 10,
+            close: 23,
+        },
+        fri: {
+            open: 10,
+            close: 23,
+        },
+        sun: {
+            open: 12,
+            close: 23,
+        },      
+    },
     orderFood(appetizersIndex, mainMenuIndex) {
         return [
             this.appetizers[appetizersIndex],
@@ -14,6 +27,7 @@ const japaneseRestaurant = {
         ]
     }
 };
-
-const [appetizer, main] = japaneseRestaurant.orderFood(3, 1)
+const x = 'fjdsk'
+const {wed:timeOfWorking} = japaneseRestaurant.workingHours;
+console.log(timeOfWorking);
 
