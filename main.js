fd0aec061e51a601822c99fed68ddd786a1aad2a
Lol <jfd@fjd>
2024-11-11 10:44:50 +0200

    final

--- main.js
+++ main.js
@@ -4,15 +4,18 @@ document.body.append(document.createElement('button'))
 
 
 
-document.querySelector('button').addEventListener('click', function(){
+document.querySelector('button').addEventListener('click', function () {
     const text = document.querySelector('textarea').value
     // console.log(text);
     const textArray = text.split('\n')
     // console.log(textArray);
     const additedArray = []
-    for(const el of textArray){
-        const elTrimSplit = el.trim().toLowerCase().split('_')
-        console.log(elTrimSplit[0] + elTrimSplit[1][0].toUpperCase() + elTrimSplit[1].slice[1]);
+    for (const el of textArray) {
+        if (el.length > 1) {
+            const elTrimSplit = el.trim().toLowerCase().split('_')
+            console.log(elTrimSplit[0] + elTrimSplit[1][0].toUpperCase() + elTrimSplit[1].slice[1]);
+        }
+
     }
 
 })
